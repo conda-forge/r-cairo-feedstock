6cfa92adb65a3fe338eb93f4f56e7bf1c36c2940
Mervin Fansler <mervin.fansler@bric.ku.dk>
2025-04-08 16:26:08 +0200

    revert

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-echo ">>> DEBUGGING START >>>"
-set -x
-ls -lh $PREFIX/lib/pkgconfig
-ls -lh $PREFIX/*/pkgconfig
-echo "<<< DEBUGGING END <<<"
-
-#export CAIRO_CFLAGS="-I${PREFIX}/include/cairo"
-#export CAIRO_LIBS="-lcairo -lpng -lz"
-#export PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig:${PKG_CONFIG_PATH}"
 export DISABLE_AUTOBREW=1
 
 # shellcheck disable=SC2086
