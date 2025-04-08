b6cff0ac60c5d3e9a1e7396b396f52aaa82d2752
Mervin Fansler <mervin.fansler@bric.ku.dk>
2025-04-08 14:58:44 +0200

    debug

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+echo ">>> DEBUGGING START >>>"
+set -x
+ls -lh $PREFIX/lib/pkgconfig
+ls -lh $PREFIX/*/pkgconfig
+echo "<<< DEBUGGING END <<<"
+
 export PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig:PKG_CONFIG_PATH"
 export DISABLE_AUTOBREW=1
 
