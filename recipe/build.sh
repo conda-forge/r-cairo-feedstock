d1fb7869c5812b656a583a424366c69890e2f61d
Mervin Fansler <mervin.fansler@bric.ku.dk>
2025-04-08 16:15:37 +0200

    retry without env vars

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,9 +6,9 @@ ls -lh $PREFIX/lib/pkgconfig
 ls -lh $PREFIX/*/pkgconfig
 echo "<<< DEBUGGING END <<<"
 
-export CAIRO_CFLAGS="-I${PREFIX}/include/cairo"
-export CAIRO_LIBS="-lcairo -lpng -lz"
-export PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig:${PKG_CONFIG_PATH}"
+#export CAIRO_CFLAGS="-I${PREFIX}/include/cairo"
+#export CAIRO_LIBS="-lcairo -lpng -lz"
+#export PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig:${PKG_CONFIG_PATH}"
 export DISABLE_AUTOBREW=1
 
 # shellcheck disable=SC2086
